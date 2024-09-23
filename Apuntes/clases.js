aae8fc3a63951a669b5c3d7edd1bc1e2d538ddea
nicholelouiss <nicholelouiss@gmail.com>
2024-09-23 14:36:49 +0100

    apuntes de clases

---
+++ Apuntes/clases.js
@@ -0,0 +1,5 @@
+var [nombre, edad] = ['pablo', 25]
+console.log(nombre, edad)
+
+var HeaderTitle = document.getElementById('header-title');
+HeaderTitle.innerHTML = '<boton> id="prueba"> Prueba de inserción de código<boton>'
