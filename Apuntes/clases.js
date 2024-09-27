4af973bf128e73660f147e5c98604156d41a639e
nicholelouiss <nicholelouiss@gmail.com>
2024-09-27 20:03:47 +0100

    dew

--- Apuntes/clases.js
+++ Apuntes/clases.js
@@ -3,3 +3,20 @@ console.log(nombre, edad)
 
 var HeaderTitle = document.getElementById('header-title');
 HeaderTitle.innerHTML = '<boton> id="prueba"> Prueba de inserción de código<boton>'
+
+
+// Estructura del fetch
+
+const url = 'https://pokeapi.co/api/v2/pokemon/1/'
+fetch(url)
+    .then((response) => response.jason())
+    .then(data => console.log('Me llaman para mi turno'));
+
+const url2 = 'https://pokeapi.co/api/v2/pokemon/1/'
+fetch(url2)
+    .then((response) => response.jason())
+    .then(data => console.log('Me llaman para mi turno'));
+console.log('Me pongo en la cola')
+
+//await esperamos a que se resuelva la promera 
+// fetch dentro de una funcion asyn
