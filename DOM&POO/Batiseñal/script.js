68358d7e516e9ea80c1c21de2dae472f9668d732
nicholelouis <nicholelouiss@gmail.com>
2024-10-18 23:55:15 +0100

    ejercicios

---
+++ DOM&POO/Batiseñal/script.js
@@ -0,0 +1,3 @@
+document.getElementById('btn').addEventListener('click', function(){
+    document.body.style.backgroundImage = "url('./bat.jpg')";
+})
---
+++ DOM&POO/EscudoCapitanAmerica/script.js
@@ -0,0 +1,13 @@
+let img = document.getElementById('img');
+let posicionX = 0;
+
+img.onclick= function(){
+    posicionX += 20;
+    img.style.left = posicionX + 'px';
+}
+
+
+
+
+
+
